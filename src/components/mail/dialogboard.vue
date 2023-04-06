a5a547bf048c645195233b40656f0bb8b0d6b628
Introvert696 <36341190+Introvert696@users.noreply.github.com>
2023-04-06 10:50:37 +0300

    сделал вывод диалогов

--- src/components/mail/dialogboard.vue
+++ src/components/mail/dialogboard.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="dialog-board">
+  <div class="dialog-board" @click="goToDialog">
     <img :src="avatar" alt="user photo" width="50" height="50" />
     <div class="text-dialog-board">
       <p>{{ namelastname }}</p>
       <span>{{ message }}</span>
     </div>
     <div class="dialog-board-delete">
-      <a href="#">Удалить</a>
+      <a :href="id">Удалить</a>
     </div>
   </div>
 </template>
@@ -14,5 +14,10 @@
 export default {
   name: "dialogboard",
   props: ["namelastname", "message", "avatar", "id"],
+  methods: {
+    goToDialog() {
+      this.$router.push("mail/" + this.id);
+    },
+  },
 };
 </script>
--- src/views/MailView.vue
+++ src/views/MailView.vue
@@ -2,29 +2,20 @@
   <mainlayout>
     <searchdialog />
     <dialogboard
-      avatar="https://www.meme-arsenal.com/memes/a2c725c61ba1d3aacd43906785171756.jpg"
-      namelastname="Алексей Пушкин"
-      message="Как дела у тебя?"
-    />
-    <dialogboard
-      avatar="https://www.meme-arsenal.com/memes/a2c725c61ba1d3aacd43906785171756.jpg"
-      namelastname="Алексей Пушкин"
-      message="Как дела у тебя?"
-    />
-    <dialogboard
-      avatar="https://www.meme-arsenal.com/memes/a2c725c61ba1d3aacd43906785171756.jpg"
-      namelastname="Алексей Пушкин"
-      message="Как дела у тебя?"
-    />
-    <dialogboard
-      avatar="https://www.meme-arsenal.com/memes/a2c725c61ba1d3aacd43906785171756.jpg"
-      namelastname="Алексей Пушкин"
-      message="Как дела у тебя?"
-    />
-    <dialogboard
-      avatar="https://www.meme-arsenal.com/memes/a2c725c61ba1d3aacd43906785171756.jpg"
-      namelastname="Алексей Пушкин"
-      message="Как дела у тебя?"
+      v-for="dialog in dialogs"
+      :key="dialog"
+      :id="dialog.id"
+      :avatar="getImage(dialog.firstuser.user_photo)"
+      :namelastname="
+        dialog.firstuser.name +
+        ' ' +
+        dialog.firstuser.lastname +
+        ' и  ' +
+        dialog.seconduser.name +
+        ' ' +
+        dialog.seconduser.lastname
+      "
+      message="Сообщение"
     />
   </mainlayout>
 </template>
@@ -32,12 +23,39 @@
 import mainlayout from "./layout/mainlayout.vue";
 import searchdialog from "@/components/mail/searchdialog.vue";
 import dialogboard from "@/components/mail/dialogboard.vue";
+import axios from "axios";
+import globals from "@/globals";
 export default {
   name: "MailView",
   components: {
     searchdialog,
     dialogboard,
     mainlayout,
+  },
+  data() {
+    return {
+      dialogs: {},
+    };
+  },
+  methods: {
+    getDialogs() {
+      axios
+        .get(globals.API_URL + "messages_groups", {
+          headers: {
+            Authorization: `Bearer ${localStorage.token}`,
+          },
+        })
+        .then((resp) => {
+          this.dialogs = resp.data;
+        });
+    },
+    getImage(image) {
+      let imageURL = globals.API_URL + "image/" + image;
+      return imageURL;
+    },
+  },
+  mounted() {
+    this.getDialogs();
   },
 };
 </script>
