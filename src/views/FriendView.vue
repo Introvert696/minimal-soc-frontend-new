296924ac04d35a0f9834f3303853fdc0c28d2afa
Introvert696 <36341190+Introvert696@users.noreply.github.com>
2023-04-26 09:05:52 +0300

    сделал вывод друзей

--- src/views/FriendView.vue
+++ src/views/FriendView.vue
@@ -2,36 +2,65 @@
   <mainlayout>
     <searchuserform />
     <div class="friends-list">
-      <friendboard
-        namelastname="Иван Филатов"
-        avatar="https://oir.mobi/uploads/posts/2021-04/1619695857_9-oir_mobi-p-glad-valakas-krolik-zhivotnie-krasivo-foto-9.jpg"
-      />
-      <friendboard
-        namelastname="Иван Филатов"
-        avatar="https://oir.mobi/uploads/posts/2021-04/1619695857_9-oir_mobi-p-glad-valakas-krolik-zhivotnie-krasivo-foto-9.jpg"
-      />
-      <friendboard
-        namelastname="Иван Филатов"
-        avatar="https://oir.mobi/uploads/posts/2021-04/1619695857_9-oir_mobi-p-glad-valakas-krolik-zhivotnie-krasivo-foto-9.jpg"
-      />
-      <friendboard
-        namelastname="Иван Филатов"
-        avatar="https://oir.mobi/uploads/posts/2021-04/1619695857_9-oir_mobi-p-glad-valakas-krolik-zhivotnie-krasivo-foto-9.jpg"
-      />
+      <div v-for="fr in friends" :key="fr">
+        <friendboard
+          v-if="fr.first_user != id"
+          :namelastname="fr.firstuser.name + ' ' + fr.firstuser.lastname"
+          :avatar="getPhotoUrl(fr.firstuser.user_photo)"
+        />
+        <friendboard
+          v-if="fr.second_user != id"
+          :namelastname="fr.seconduser.name + ' ' + fr.seconduser.lastname"
+          :avatar="getPhotoUrl(fr.seconduser.user_photo)"
+        />
+      </div>
     </div>
   </mainlayout>
 </template>
 
 <script>
+//http://127.0.0.1:8000/api/friends
 import mainlayout from "./layout/mainlayout.vue";
 import searchuserform from "@/components/friend/searchuserform.vue";
 import friendboard from "@/components/friend/friendboard.vue";
+import axios from "axios";
+import globals from "@/globals";
 export default {
   name: "FriendView",
   components: {
     searchuserform,
     friendboard,
     mainlayout,
+  },
+  data() {
+    return {
+      friends: [],
+      id: 0,
+    };
+  },
+  methods: {
+    getFreinds() {
+      axios
+        .get(globals.API_URL + "friends", {
+          headers: {
+            Authorization: `Bearer ${localStorage.token}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          this.friends = response.data;
+        })
+        .catch((resp) => {
+          console.log(resp);
+        });
+    },
+    getPhotoUrl(name) {
+      return globals.API_URL + "image/" + name;
+    },
+  },
+  mounted() {
+    this.getFreinds();
+    this.id = localStorage.id;
   },
 };
 </script>
