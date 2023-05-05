1dd914a838e1e9500a0d3329c3a0a9cfaaff12d0
Introvert696 <36341190+Introvert696@users.noreply.github.com>
2023-05-05 13:24:24 +0400

    фикшу настройки (недофиксил)

--- src/components/setting/inputform.vue
+++ src/components/setting/inputform.vue
@@ -1,31 +1,31 @@
 <template>
-  <form action="#" class="setting-form">
+  <div class="setting-form">
     <div class="setting-input">
       <label for="name">Имя:</label><br />
-      <input type="text" name="name" :value="name" />
+      <input type="text" name="name" v-model="input_name" />
     </div>
     <div class="setting-input">
       <label for="login">Логин:</label><br />
-      <input type="text" name="login" :value="username" />
+      <input type="text" name="login" v-model="input_login" />
     </div>
     <div class="setting-input">
       <label for="latname">Фамилия:</label><br />
-      <input type="text" name="latname" />
+      <input type="text" name="latname" v-model="input_lastname" />
     </div>
     <div class="setting-input">
       <label for="name">Дата рождения:</label><br />
-      <input type="date" name="name" :value="datecreate" />
+      <input type="date" name="name" v-model="input_datecreate" />
     </div>
     <div class="setting-input">
       <label for="password">Пароль:</label><br />
-      <input type="password" name="password" />
+      <input type="password" name="password" v-model="input_password" />
     </div>
     <div class="setting-input">
       <label for="password2">Повторите пароль:</label><br />
       <input type="password" name="password2" />
     </div>
     <div class="setting-btn">
-      <input type="file" name="file" id="input_file" />
+      <input type="file" name="file" id="input_file" @input="setUserPhoto" />
       <label for="input_file" class="setting-img-btn">
         <span>Выбрать изображение</span>
       </label>
@@ -33,24 +33,62 @@
     <br />
 
     <div class="setting-btn">
-      <input type="file" name="bg" id="bg" />
+      <input type="file" name="bg" id="bg" @input="setUserBG" />
       <label for="bg" class="setting-img-btn">
         <span>Выбрать фон профиля</span>
       </label>
     </div>
     <br />
 
-    <button type="submit" class="setting-save-btn">Сохранить</button>
-  </form>
+    <button @click="updateInfo" class="setting-save-btn">Сохранить</button>
+  </div>
 </template>
 <script>
+import axios from "axios";
+import globals from "@/globals";
 export default {
   name: "inputform",
-  props: {
-    name: String,
-    lastname: String,
-    username: String,
-    datecreate: Date,
+  data() {
+    return {
+      input_name: "",
+      input_login: "",
+      input_lastname: "",
+      input_username: "",
+      input_password: "",
+      input_datecreate: "",
+      input_user_photo: {},
+      input_bg_photo: {},
+    };
   },
+  methods: {
+    updateInfo() {
+      var updateData = new FormData();
+      updateData.append("photo_user", this.input_user_photo);
+      console.log(localStorage.token);
+      axios
+        .post(
+          globals.API_URL + "users/" + localStorage.id + "?_method=PUT",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.token}`,
+            },
+          },
+          updateData
+        )
+        .then((resp) => {
+          console.log(resp);
+        })
+        .catch((resp) => {
+          console.log(resp);
+        });
+    },
+    setUserPhoto(file) {
+      this.input_user_photo = file;
+    },
+    setUserBG(file) {
+      this.input_bg_photo = file;
+    },
+  },
+  mounted() {},
 };
 </script>
